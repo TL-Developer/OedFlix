dfc0675d87d6a48545f5a27f3835577832acf88b
Tiago Lima <tiago.jlima@yahoo.com.br>
2016-01-29 12:22:16 -0200

    implementando dropzone

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -4,7 +4,8 @@ module.exports = function(app){
   var controller = {};
 
   controller.sendOed = function(req, res, next){
-
+    console.log(req);
+    res.end()
   };
 
   return controller;
--- public/js/controllers/DashCadastrarController.js
+++ public/js/controllers/DashCadastrarController.js
@@ -1,5 +1,19 @@
 angular.module('OedFlix').controller('DashCadastrarController',['$scope','Oeds', function($scope, Oeds){
 
-
+  $scope.dropzoneConfig = {
+    'options': {
+      'url': '/dashboard/sendOed'
+    },
+    'eventHandlers': {
+      'sending': function (file, xhr, formData) {
+        console.log('sending');
+      },
+      'success': function (file, response) {
+        console.log('success');
+        console.log(file);
+        console.log(response);
+      }
+    }
+  };
 
 }]);
